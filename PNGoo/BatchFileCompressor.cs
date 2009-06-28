76432f9e471795a8f371b63975b4d7c23b737235
jaffathecake@gmail.com <jaffathecake@gmail.com@79186a3a-640d-11de-89e8-69fd2269e136>
2009-06-28 18:07:14 +0000

    Initial commit

---
+++ PNGoo/BatchFileCompressor.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PNGoo
+{
+    public class BatchFileCompressor
+    {
+        /// <summary>
+        /// Paths of files to compress
+        /// </summary>
+        public string[] FilePaths;
+
+        /// <summary>
+        /// Directory to output compressed files. Set as null to output to original files' directory.
+        /// </summary>
+        public string OutputDirectory = "";
+
+        /// <summary>
+        /// Should the file be output even if it's larger? Otherwise the original will be output
+        /// </summary>
+        public bool OutputIfLarger = false;
+
+        /// <summary>
+        /// Setting to use to compress files
+        /// </summary>
+        public CompressionSettings CompressionSettings;
+
+        /// <summary>
+        /// Fired when a particular file has been processed, even if compression failed
+        /// </summary>
+        public event EventHandler FileProcessed;
+
+        public BatchFileCompressor() {}
+
+        /// <summary>
+        /// Begin compressing files
+        /// </summary>
+        public void Start()
+        {
+            if (FilePaths == null || OutputDirectory == String.Empty)
+            {
+                throw new Exception("FilePaths / OutputDirectory not set");
+            }
+
+            // loop through our files
+            foreach (string filePath in FilePaths)
+            {
+                try
+                {
+                    PNGCompressor pngCompressor = compress(filePath);
+                    byte[] fileToWrite = pngCompressor.CompressedFile;
+                    string outputDirectory = OutputDirectory;
+                    // we may be getting a jpg as input, make sure we output png
+                    string fileName = Path.GetFileNameWithoutExtension(filePath) + ".png";
+
+                    // if the compressed file is larger than the original, keep the original (unless told otherwise)
+                    // TODO: stop this from outputting non-pngs as pngs
+                    if (!OutputIfLarger && pngCompressor.CompressedFile.Length > pngCompressor.OriginalFile.Length)
+                    {
+                        fileToWrite = pngCompressor.OriginalFile;
+                    }
+
+                    // we're going to output to the same directory, overwriting files if needed
+                    if (outputDirectory == null)
+                    {
+                        outputDirectory = Path.GetDirectoryName(filePath);
+                    }
+
+                    string outputFilePath = outputDirectory + "/" + fileName;
+
+                    // output the file
+                    File.WriteAllBytes(outputDirectory + "/" + fileName, fileToWrite);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compress an image according to given settings
+        /// </summary>
+        /// <param name="filePath">Path of the file to compress</param>
+        /// <returns>Compressed PNG Object</returns>
+        private PNGCompressor compress(string filePath)
+        {
+            // read file
+            byte[] fileData = File.ReadAllBytes(filePath);
+
+            // select which compressor to use
+            switch (this.CompressionSettings.OutputType)
+            {
+                case CompressionSettings.PNGType.Indexed:
+                    return compressIndexed(fileData);
+                default:
+                    throw new Exception("Invalid output type");
+            }
+        }
+
+        /// <summary>
+        /// Compress an image according to given indexed settings
+        /// </summary>
+        /// <param name="fileData">Byte data of the file to compress</param>
+        /// <returns>Compressed PNGCompressor</returns>
+        private PNGCompressor compressIndexed(byte[] fileData)
+        {
+            PNGQuant pngQuant = new PNGQuant(fileData);
+            pngQuant.CompressionSettings = this.CompressionSettings.Indexed;
+            pngQuant.Start();
+            return pngQuant;
+        }
+
+        private void OnFileProcessed(/* need event args */)
+        {
+        }
+    }
+}
---
+++ PNGoo/MainView.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Drawing.Imaging;
+
+namespace PNGoo
+{
+    public partial class MainView : Form
+    {
+        private delegate void passMsg(string msg);
+
+        /// <summary>
+        /// Settings to use for batch file processing
+        /// </summary>
+        private static CompressionSettings compressionSettings = new CompressionSettings();
+
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void MainView_Load(object sender, EventArgs e)
+        {
+            // add compression types
+            string[] enumNames = Enum.GetNames(typeof(CompressionSettings.PNGType));
+            this.pngTypeComboBox.Items.AddRange(enumNames);
+            this.pngTypeComboBox.SelectedIndex = 0;
+        }
+
+        private void addFilesButton_Click(object sender, EventArgs e)
+        {
+            if (addFilesDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in addFilesDialog.FileNames)
+                {
+                    addFileToBatch(file);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds a file to fileBatchDataGridView.
+        /// TODO: Prevent adding duplicates
+        /// </summary>
+        /// <param name="path">Path to file</param>
+        /// <param name="display">Displayed 'path'</param>
+        private void addFileToBatch(string path, string display)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            // we don't accept directories (yet)
+            if (fileInfo.Attributes == FileAttributes.Directory)
+            {
+                return;
+            }
+            // search for duplicates of the current file
+            foreach (DataGridViewRow row in fileBatchDataGridView.Rows)
+            {
+
+                if (row.Cells["RealFileColumn"].Value.ToString() == path)
+                {
+                    return;
+                }
+            }
+            long fileSize = new FileInfo(path).Length;
+            double fileK = fileSize / 1024;
+            fileK = Math.Round(fileK, 2);
+            fileBatchDataGridView.Rows.Add(display, path, fileK + "k", "", "Uncompressed");
+        }
+        /// <summary>
+        /// Adds a file to fileBatchDataGridView.
+        /// </summary>
+        /// <param name="path">Path to file</param>
+        private void addFileToBatch(string path)
+        {
+            addFileToBatch(path, path);
+        }
+
+        private void MainView_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void MainView_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                addFileToBatch(file);
+            }
+        }
+
+        private void pasteMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Collections.Specialized.StringCollection files = Clipboard.GetFileDropList();
+            foreach (string file in files)
+            {
+                addFileToBatch(file);
+            }
+        }
+
+        /// <summary>
+        /// Pastes files from the clipboard into the datagrid
+        /// </summary>
+        private void pasteFiles()
+        {
+            if (canPasteFiles())
+            {
+                System.Collections.Specialized.StringCollection files = Clipboard.GetFileDropList();
+                foreach (string file in files)
+                {
+                    addFileToBatch(file);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Can files be pasted from the clipboard?
+        /// </summary>
+        /// <returns>true if files can be pasted</returns>
+        private bool canPasteFiles()
+        {
+            return Clipboard.ContainsFileDropList();
+        }
+
+        private void removeItemButton_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in fileBatchDataGridView.SelectedRows)
+            {
+                fileBatchDataGridView.Rows.Remove(row);
+            }
+        }
+
+        private void fileBatchDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            removeItemButton.Enabled = (fileBatchDataGridView.SelectedRows.Count != 0);
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pasteFiles();
+        }
+
+        private void MainView_Activated(object sender, EventArgs e)
+        {
+            bool canPaste = canPasteFiles();
+            pasteMenuItem.Enabled = canPaste;
+            pasteToolStripMenuItem.Enabled = canPaste;
+        }
+
+        private void outputDirectoryBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (outputFolderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputDirectoryTextBox.Text = outputFolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void colourSettingsButton_Click(object sender, EventArgs e)
+        {
+            ColourSettings colourSettings;
+            string filePath;
+
+            DataGridViewSelectedRowCollection selectedRows = fileBatchDataGridView.SelectedRows;
+
+            // bug out if there aren't any rows selected
+            if (selectedRows.Count == 0)
+            {
+                MessageBox.Show(
+                    "To prevew colour settings, you must first select an image to preview from the list below",
+                    "No Image Selected",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+            filePath = fileBatchDataGridView.SelectedRows[0].Cells["RealFileColumn"].Value.ToString();
+            try
+            {
+                colourSettings = new ColourSettings(filePath);
+                colourSettings.CompressionSettings = compressionSettings.Indexed;
+                colourSettings.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    "The selected image could not be loaded.",
+                    "Invalid Image",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (colourSettings.DialogResult == DialogResult.OK)
+            {
+                compressionSettings.Indexed = colourSettings.CompressionSettings;
+            }
+            
+        }
+
+        private void overwriteCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            outputDirectoryTextBox.Enabled =
+                outputDirectoryBrowseButton.Enabled =
+                !overwriteCheckBox.Checked;
+        }
+
+        private void pngTypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            compressionSettings.OutputType = (CompressionSettings.PNGType)pngTypeComboBox.SelectedIndex;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            BatchFileCompressor batch = new BatchFileCompressor();
+            batch.OutputDirectory = outputDirectoryTextBox.Text;
+            batch.OutputIfLarger = overwriteIfLargerCheckBox.Checked;
+            batch.CompressionSettings = compressionSettings;
+            batch.FilePaths = getFileList();
+
+            // if we're wanting to overwrite the original, set the output dir to null
+            if (overwriteCheckBox.Checked)
+            {
+                batch.OutputDirectory = null;
+            }
+
+            batch.Start();
+        }
+
+        /// <summary>
+        /// Gets the list of files added to the box
+        /// </summary>
+        /// <returns>List of files</returns>
+        private string[] getFileList()
+        {
+            string[] fileList = new string[fileBatchDataGridView.Rows.Count];
+            int i = 0;
+
+            foreach (DataGridViewRow row in fileBatchDataGridView.Rows)
+            {
+                fileList[i++] = row.Cells["RealFileColumn"].Value.ToString();
+            }
+            return fileList;
+        }
+
+    }
+}
